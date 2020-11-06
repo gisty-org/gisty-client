707c7e8520f21088906f1b29bafb634da670b865
Saif Kazi <saif1204kazi@gmail.com>
2020-11-07 00:06:42 +0530

    Finally Fixed reload 404 error

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
 const history = require('connect-history-api-fallback');
+
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+
 app.use(history({
-    "rewrites": [
-        { "from": '/*', "to": '/index.html'}
-    ]
+    disableDotRule: true,
+    htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
 }));
-const port = process.env.PORT || 80;
+
+app.use(express.static(path.join(__dirname, '/dist')));
+
+const port = process.env.PORT || 8080;
 app.listen(port);

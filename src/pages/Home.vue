59af9897636e470f8e9014905b09364987019f9f
Saif Kazi <saif1204kazi@gmail.com>
2020-11-15 20:52:56 +0530

    Updated Sidebar UI

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,10 +3,10 @@
     <div>
         <base-spinner :show="isLoading"></base-spinner>
         <div class="grid grid-cols-5 grid-rows-1 h-screen" >
-            <div class="col-span-1 bg-gray-900 pt-12 relative shadow-2xl">
+            <div class="col-span-1 pt-12 relative shadow-2xl" style="background-color: #667eea;">
                 <div class="pb-6">
-                    <div class="rounded-full h-32 w-32 mx-auto" style="background-color: #667eea;">
-                        <h1 class="text-center p-4 text-6xl text-white">{{ fullName[0] }}</h1>
+                    <div class="rounded-full h-32 w-32 mx-auto bg-white" >
+                        <h1 class="text-center p-4 text-6xl" style="color: #667eea;">{{ fullName[0] }}</h1>
                     </div>
                     <div class="mx-auto text-center mt-5">
                         <p class="text-white font-semibold text-xl">{{ 'Hello, ' + fullName }}</p>
@@ -17,18 +17,18 @@
                         <li 
                             v-for="subject in allSubjects"
                             :key="subject" 
-                            class="text-white font-semibold text-lg px-3 py-3 align-middle item-hover"
+                            class="text-white font-semibold text-lg px-3 py-3 align-middle item-hover rounded-lg m-3"
                             :class="{ isSelected: isCurrentFolder(subject) }"
                             @click="select(subject)"
                         >
-                            <b-icon-folder-fill class="inline-block mr-2 mb-1 text-lg" v-if="isCurrentFolder(subject)"></b-icon-folder-fill>
-                            <b-icon-folder class="inline-block mr-2 mb-1 text-lg" v-else></b-icon-folder>
+                            <b-icon-folder-fill class="inline-block mr-2 mb-1 text-lg"></b-icon-folder-fill>
+                            <!-- <b-icon-folder class="inline-block mr-2 mb-1 text-lg"></b-icon-folder> -->
                             {{ subject }}
                         </li>
                     </ul>
                 </div>
-                <div class="h-12 inset-x-0 absolute bottom-0 p-0" @click="logout">
-                    <div class="logout-div text-xl">
+                <div class="inset-x-0 absolute bottom-0 p-0" @click="logout">
+                    <div class="logout-div text-xl h-12 font-bold">
                         <b-icon-arrow-bar-left class="inline-block mr-2 mb-1 text-xl"></b-icon-arrow-bar-left>Logout
                     </div>
                 </div>
@@ -96,11 +96,10 @@ export default {
 .logout-div {
     @apply px-3;
     @apply py-2;
-    @apply border-t;
-    @apply border-gray-900;
-    @apply border-opacity-50;
-    background-color: #667eea;
-    @apply text-white;
+    @apply m-3;
+    background-color: white;
+    @apply rounded-lg;
+    color: #667eea;
 }
 
 .logout-div a{
@@ -110,8 +109,8 @@ export default {
 }
 
 .isSelected {
-    background-color: #667eea;
-    @apply text-white;
+    background-color:white;
+    color: #667eea;
 }
 
 .sidebar{
@@ -124,8 +123,8 @@ export default {
 }
 
 .item-hover:hover {
-    background-color: #667eea;
-    @apply text-white;
+    background-color:white;
+    color: #667eea;
 }
 
 </style>

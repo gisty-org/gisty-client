66f033eadc0f4e7e36d337957a555bc2401923f1
Saif Kazi <saif1204kazi@gmail.com>
2020-11-18 20:34:04 +0530

    Updated testimonials

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -56,19 +56,19 @@
                 <div class="flex flex-wrap">
                 <div class="w-full md:w-1/3 px-2 mb-4">
                     <div class="bg-white rounded-lg shadow py-3">
-                        <p class="text-gray-800 text-base px-6 mb-5">Monitoring and tracking my health vitals anywhere I go and on any platform I use has never been easier.</p>
+                        <p class="text-gray-800 text-base px-6 mb-5">"In a hectic environment, it not only helps to summarize the meeting but also organizes it well."</p>
                         <p class="text-gray-500 text-xs md:text-sm px-6">John Doe</p>
                     </div>
                 </div>
                 <div class="w-full md:w-1/3 px-2 mb-4">
                     <div class="bg-white rounded-lg shadow py-3">
-                        <p class="text-gray-800 text-base px-6 mb-5">As an Athlete, this is the perfect product for me. I wear my Smart Health Monitoring Wristwatch everywhere I go, even in the bathroom since it's waterproof.</p>
+                        <p class="text-gray-800 text-base px-6 mb-5">"In such a busy schedule of my college, it helps to get the summary of all the meets and save my time. A big thanks to gisty."</p>
                         <p class="text-gray-500 text-xs md:text-sm px-6">Jane Doe</p>
                     </div>
                 </div>
                 <div class="w-full md:w-1/3 px-2 mb-4">
                     <div class="bg-white rounded-lg shadow py-3">
-                        <p class="text-gray-800 text-base px-6 mb-5">I don't regret buying this wearble gadget. One of the best gadgets I own!.</p>
+                        <p class="text-gray-800 text-base px-6 mb-5">"I was sick for a month, it has helped immensely to recover lost studies. Hats off to gisty!"</p>
                         <p class="text-gray-500 text-xs md:text-sm px-6">James Doe</p>
                     </div>
                 </div>
